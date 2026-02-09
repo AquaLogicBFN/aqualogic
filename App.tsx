d88a27f798d6145646cb5fdca1e816cb3486827e
AquaLogic <aqualogiclandscaping@gmail.com>
2026-02-09 11:25:30 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import ServiceDetail from './components/ServiceDetail';
-import Footer from './components/Footer';
+import Navbar from "@/components/Navbar";
+import Home from "@/components/Home";
+import ServiceDetail from "@/components/ServiceDetail";
+import Footer from "@/components/Footer";
 
 function App() {
   return (
